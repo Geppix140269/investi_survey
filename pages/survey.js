5a79a62414e5821c3dba99ae130d648f29e93503
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-01 14:23:23 +0200

    Create survey.js

---
+++ pages/survey.js
@@ -0,0 +1,8 @@
+// Copy the ENTIRE property-survey-complete artifact here
+export default function Survey() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: `
+      <!-- PASTE COMPLETE PROPERTY SURVEY HTML HERE -->
+    `}} />
+  );
+}
